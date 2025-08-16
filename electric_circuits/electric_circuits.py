12012b6facb89e273610f6faa1a42eeba4690223
MattiaBenincasa <mattia.benincasa03@gmail.com>
2025-08-16 14:59:29 +0200

    Refactored circuit clustering tests to manage multiple execution of tests

--- electric_circuits/electric_circuits.py
+++ electric_circuits/electric_circuits.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.signal import cont2discrete, dlsim
 from matplotlib import pyplot as plt
 
+
 # The circuit topology implemented here follows the configuration described in:
 # O. Lauwers, B. De Moor, "A time series distance measure for efficient clustering of input/output
 # signals by their underlying dynamics", see Figure 1.
@@ -47,6 +48,50 @@ def generate_sinusoidal_signal(n_samples, n_signals=1):
         return 10 * np.sin(2 * np.pi * f * n * Ts) + np.random.normal(0, 0.2, size=n_samples)
 
 
+def generate_multi_sine_wave(n_samples):
+    # sampling parameters
+    fs = 400
+    Ts = 1 / fs
+    n = np.arange(n_samples)
+
+    n_sinusoid = 3
+    max_amp = 5.0
+    min_amp = 0.5
+    max_freq = 0.01
+    min_freq = 0.001
+    min_phase = 0
+    max_phase = 2 * np.pi
+
+    sinusoidal_signals = []
+
+    for sinusoid in range(n_sinusoid):
+        sinusoidal_signals.append({
+            'amplitude': np.random.uniform(min_amp, max_amp),
+            'freq': np.random.uniform(min_freq, max_freq),
+            'phase': np.random.uniform(min_phase, max_phase)
+        })
+
+    multi_sine = np.zeros(n_samples)
+
+    for sinusoid in sinusoidal_signals:
+        amplitude = sinusoid['amplitude']
+        freq = sinusoid['freq']
+        fase = sinusoid['phase']
+        multi_sine += amplitude * np.sin(2 * np.pi * freq * n * Ts + fase) + np.random.normal(0, 0.1, size=n_samples)
+
+    return multi_sine
+
+
+def generate_multiple_multi_sin_waves(n_samples, n_signals):
+    if n_signals > 1:
+        signals = []
+        for _ in range(n_signals):
+            signals.append(generate_multi_sine_wave(n_samples))
+        return signals
+    else:
+        return generate_multi_sine_wave(n_samples)
+
+
 # simulate one circuit on more inputs
 def simulate_circuit_on_multiple_input(input_signals, circuit):
     output_signals = []
@@ -72,13 +117,13 @@ def simulate_circuit_on_multiple_inputs_with_output_noise(input_signals, circuit
     clean_outputs = simulate_circuit_on_multiple_input(input_signals, circuit)
     noisy_output = []
     for output in clean_outputs:
-        noisy_output.append(generate_noise_from_SNR(output, snr))
+        noisy_output.append(generate_noise_from_snr(output, snr))
 
     return noisy_output
 
 
-def generate_noise_from_SNR(signal, snr):
+def generate_noise_from_snr(signal, snr):
     power_signal = np.mean(signal**2)
-    power_noise = power_signal / (10 ** (snr/ 10))
+    power_noise = power_signal / (10 ** (snr / 10))
     noise = np.random.normal(0, np.sqrt(power_noise), size=signal.size)
     return signal + noise
--- electric_circuits/test_clustering.py
+++ electric_circuits/test_clustering.py
@@ -2,7 +2,9 @@ from electric_circuits.electric_circuits import (generate_discrete_lti_circuit,
                                                  generate_white_noise_signal,
                                                  simulate_circuit_on_multiple_input,
                                                  generate_sinusoidal_signal,
-                                                 simulate_circuit_on_multi_input_with_x0)
+                                                 simulate_circuit_on_multi_input_with_x0,
+                                                 generate_multiple_multi_sin_waves,
+                                                 simulate_circuit_on_multiple_inputs_with_output_noise)
 from clustering import generate_dataset_circuit, k_means, compute_and_plot_conf_matrix
 from sklearn.metrics.cluster import adjusted_rand_score
 import numpy as np
@@ -29,99 +31,133 @@ def init_circuits():
 
 
 def test_two_circuits_clustering():
-    # simulation setup parameters
-    n_samples = 2**10
-    n_input_signals = 100
-
+    # simulation parameters setup
+    n_samples = [2**10, 2**12, 2**14]
     sys_1, sys_2 = init_circuits()
+    r = 1  # repetitions for each test with a specific length
+    ari_with_different_length = {}
+    for n_sample in n_samples:
+        ari = []
+        for _ in range(r):
+            # generate input signals
+            inputs = generate_white_noise_signal(n_sample, 0, 0.6, 50)
+            # inputs = generate_multiple_multi_sin_waves(n_sample, 50)
 
-    # generate white noise inputs
-    inputs_noise = generate_white_noise_signal(n_samples, 0, 0.6, n_input_signals)
+            # simulate circuits on white noise signal
+            outputs = {'system_1': simulate_circuit_on_multiple_input(inputs, sys_1),
+                       'system_2': simulate_circuit_on_multiple_input(inputs, sys_2)}
 
-    # simulate circuits on white noise signal
-    outputs_noise = {'system_1': simulate_circuit_on_multiple_input(inputs_noise, sys_1),
-                     'system_2': simulate_circuit_on_multiple_input(inputs_noise, sys_2)}
-
-    # generate sinusoidal signal
-    inputs_sinusoidal = generate_sinusoidal_signal(n_samples, n_input_signals)
-
-    # simulate circuits on sinusoidal signal
-    outputs_sinusoid = {'system_1': simulate_circuit_on_multiple_input(inputs_sinusoidal, sys_1),
-                        'system_2': simulate_circuit_on_multiple_input(inputs_sinusoidal, sys_2)}
-
-    dataset_noise, true_clusters_noise = generate_dataset_circuit(inputs_noise, outputs_noise['system_1'],
-                                                                  outputs_noise['system_2'])
-
-    dataset_sinusoid, true_clusters_sinusoid = generate_dataset_circuit(inputs_sinusoidal, outputs_sinusoid['system_1'],
-                                                                        outputs_sinusoid['system_2'])
-    centroids_noise, predicted_clusters_noise = k_means(dataset_noise, 2)
-    centroids_sinusoid, predicted_clusters_sinusoid = k_means(dataset_sinusoid, 2)
-    print(f'Two circuits n_samples: {n_samples} n_input_signals: {n_input_signals}:')
-    print(f'ARI index white noise input: {adjusted_rand_score(true_clusters_noise, predicted_clusters_noise)}')
-    print(f'ARI index sinusoidal input: {adjusted_rand_score(true_clusters_sinusoid, predicted_clusters_sinusoid)}')
-    print('----------------------------------------------')
-    compute_and_plot_conf_matrix(true_clusters_noise, predicted_clusters_noise, 'noise signal clustering')
-    compute_and_plot_conf_matrix(true_clusters_sinusoid, predicted_clusters_sinusoid, 'sinusoidal signal clustering')
+            dataset, true_clusters = generate_dataset_circuit(inputs, outputs['system_1'], outputs['system_2'])
+            centroids, predicted_clusters = k_means(dataset, 2)
+            ari.append(adjusted_rand_score(true_clusters, predicted_clusters))
+            print("computed")
+        ari_with_different_length[n_sample] = ari
+        print(f'{n_sample} computed')
+    plot_mean_and_std(compute_mean_and_std(ari_with_different_length),
+                      title="Clustering di serie temporali di lunghezza diversa",
+                      x_label="lunghezza serie temporali",
+                      y_label="indice ARI")
 
 
-def test_increasing_noise_intensity():
-
-    n_samples = 2**12
+def test_increasing_noise_intensity_clustering(n_samples):
     n_input_signals = 50
-
     sys_1, sys_2 = init_circuits()
-    sigmas = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 1]
-    ARIs = []
-    for sigma in sigmas:
-        # inputs_noise = generate_white_noise_signal(n_samples, 0, 0.6, n_input_signals)
-        inputs_noise = generate_sinusoidal_signal(n_samples, n_input_signals)
-        outputs_white_noise = {'system_1': np.add(simulate_circuit_on_multiple_input(inputs_noise, sys_1),
-                                                  np.random.normal(0, sigma, size=(n_input_signals, n_samples))),
-                               'system_2': np.add(simulate_circuit_on_multiple_input(inputs_noise, sys_2),
-                                                  np.random.normal(0, sigma, size=(n_input_signals, n_samples)))}
+    # different SNR in dB
+    snr_values = [30, 20, 15, 10, 8, 5, 1, 0.5]
+    ari_with_different_snr = {}
+    r = 5  # repetitions for each test with a specific length
+    for snr in snr_values:
+        ari = []
+        for _ in range(r):
+            inputs_noise = generate_white_noise_signal(n_samples, 0, 0.6, n_input_signals)
+            # inputs_noise = generate_sinusoidal_signal(n_samples, n_input_signals)
+            outputs_white_noise = {'system_1': simulate_circuit_on_multiple_inputs_with_output_noise(inputs_noise, sys_1, snr),
+                                   'system_2': simulate_circuit_on_multiple_inputs_with_output_noise(inputs_noise, sys_2, snr)}
 
-        dataset_noise, true_clusters_noise = generate_dataset_circuit(inputs_noise, outputs_white_noise['system_1'],
-                                                                      outputs_white_noise['system_2'])
+            dataset_noise, true_clusters_noise = generate_dataset_circuit(inputs_noise, outputs_white_noise['system_1'],
+                                                                          outputs_white_noise['system_2'])
 
-        centroids_noise, predicted_clusters_noise = k_means(dataset_noise, 2)
-        ARIs.append(adjusted_rand_score(true_clusters_noise, predicted_clusters_noise))
-        print(f'sigma {sigma} computed')
-
-    x = np.arange(len(sigmas))
-    plt.bar(x, ARIs, 0.4, label='ARI index')
-    plt.xticks(x, [str(sigma) for sigma in sigmas])
-    plt.xlabel('standard deviation measure error')
-    plt.ylabel('ARI')
-    plt.title('sinusoid in - different standard deviation')
-    plt.legend()
-    plt.show()
-
-    # print(f'Two circuits n_samples: {n_samples} n_input_signals: {n_input_signals}:')
-    # print(f'ARI index white noise input: {adjusted_rand_score(true_clusters_noise, predicted_clusters_noise)}')
-    # print('----------------------------------------------')
-    # compute_and_plot_conf_matrix(true_clusters_noise, predicted_clusters_noise, 'noise signal clustering')
+            centroids_noise, predicted_clusters_noise = k_means(dataset_noise, 2)
+            ari.append(adjusted_rand_score(true_clusters_noise, predicted_clusters_noise))
+            print(f'SNR {snr} computed')
+        ari_with_different_snr[snr] = ari
+    plot_mean_and_std(compute_mean_and_std(ari_with_different_snr),
+                      title="Clustering di serie temporali con errori di misura in output",
+                      x_label="Rumore di misura (dB)",
+                      y_label="indice ARI")
 
 
 def test_clustering_with_different_initial_conditions(sigma):
-    #   clustering of 100 (u, y) signal pairs: 50 from system 1 and 50 from system 2.
-    #   Within the group of 50 signals, there are 25 outputs generated with x(0) = 0
-    #   and 25 generated with x!=0.
-    #   Different initial conditions are generated from normal distribution with sigma
-    #   standard deviation
-
-    n_samples = 2**12
+    n_inputs_signals = 50
+    n_samples = [2**8, 2**10, 2**12, 2**14, 2**16]
     sys_1, sys_2 = init_circuits()
-    inputs = generate_white_noise_signal(n_samples, 0, 0.6, 50)
-    x0_1 = np.random.normal(0, sigma, size=(3, 25))
-    x0_2 = np.random.normal(0, sigma, size=(3, 25))
-    outputs_1_no_x0 = simulate_circuit_on_multiple_input(inputs[:25], sys_1)
-    outputs_2_no_x0 = simulate_circuit_on_multiple_input(inputs[:25], sys_2)
-    outputs_1_with_x0 = simulate_circuit_on_multi_input_with_x0(inputs[25:50], sys_1, x0_1)
-    outputs_2_with_x0 = simulate_circuit_on_multi_input_with_x0(inputs[25:50], sys_2, x0_2)
+    r = 5   # repetitions for each test with a specific length
 
-    outputs_1 = outputs_1_no_x0 + outputs_1_with_x0
-    outputs_2 = outputs_2_no_x0 + outputs_2_with_x0
+    # random initial conditions
+    x0 = np.random.normal(0, sigma, size=(3, n_inputs_signals))
+    ari_with_different_length = {}
 
-    dataset, true_clusters = generate_dataset_circuit(inputs, outputs_1, outputs_2)
-    centroids, predicted_clusters = k_means(dataset, 2)
-    print(adjusted_rand_score(true_clusters, predicted_clusters))
+    for n in n_samples:
+        ari = []
+        for _ in range(r):
+            inputs = generate_white_noise_signal(n, 0, 0.6, n_inputs_signals)
+            outputs_1 = simulate_circuit_on_multi_input_with_x0(inputs, sys_1, x0)
+            outputs_2 = simulate_circuit_on_multi_input_with_x0(inputs, sys_2, x0)
+
+            dataset, true_clusters = generate_dataset_circuit(inputs, outputs_1, outputs_2)
+            centroids, predicted_clusters = k_means(dataset, 2)
+            ari.append(adjusted_rand_score(true_clusters, predicted_clusters))
+            print("computed")
+        print(f'{n} computed')
+        ari_with_different_length[n] = ari
+    plot_mean_and_std(compute_mean_and_std(ari_with_different_length),
+                      title="Clustering con condizioni iniziali diverse da zero",
+                      x_label="lunghezza serie temporali",
+                      y_label="indice ARI")
+
+
+def compute_mean_and_std(ari_values):
+    results = {}
+    for key, ari_values in ari_values.items():
+        mean = np.mean(ari_values)
+        dev_std = np.std(ari_values)
+        results[key] = {
+            'mean': mean,
+            'dev_std': dev_std
+        }
+
+    return results
+
+
+def plot_mean_and_std(results, title, x_label, y_label):
+    keys = list(results.keys())
+    means = [result['mean'] for result in results.values()]
+    dev_std = [result['dev_std'] for result in results.values()]
+
+    lower_errors = []
+    upper_errors = []
+    for m, s in zip(means, dev_std):
+        lower = max(0, m - s)
+        upper = min(1, m + s)
+        lower_errors.append(m - lower)
+        upper_errors.append(upper - m)
+
+    asymmetric_errors = [lower_errors, upper_errors]
+
+    # Crea il grafico
+    plt.figure(figsize=(8, 6))
+    plt.bar(
+        [str(k) for k in keys],
+        means,
+        capsize=5,
+        width=0.6,
+        color='skyblue',
+        ecolor='red'
+    )
+
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.ylim(0, 1.05)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
